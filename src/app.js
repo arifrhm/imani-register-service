67aea42665639cd69a81523fae3057c567bb82cc
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-10-15 05:35:21 +0700

    initial

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// Connect to MongoDB database
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase', { useNewUrlParser: true });
+
+// Configure body parser
+app.use(bodyParser.json());
+
+// Define routes
+app.use('/api/auth', authRoutes);
+
+// Start the server
+app.listen(3000, () => console.log('Server started on port 3000'));
